f1363117b4279b4b93818c2094b6e873e58415cd
varun <aitvarunbhatia@gmail.com>
2023-10-04 15:28:33 +0530

    update

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-//TODO: working
-
 import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
 import logger from "./logger.js";
 import data from "./data.js";
-const port = process.env.PORT;
 import { getNotes, getNote, createNote } from "./database.js";
 
 const app = express();
-
+const port = process.env.PORT;
 let people = data.people;
-app.use(express.json());
-app.use(express.static("./public"));
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.json());
+app.use(express.static("./public"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./pages/index.html"));
 });
@@ -41,7 +39,7 @@ app.get("/api/people", (req, res) => {
 });
 //send response to all routes which are not defined/exist
 app.all("*", [logger], async (req, res) => {
-  res.status(404).send("Sorry this route does not exist.from app");
+  res.status(404).send("Sorry this route does not exist.");
 });
 
 app.use((err, req, res, next) => {
@@ -50,5 +48,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log("Server is running on port 8080");
+  console.log(`Server running on port ${port}`);
 });
